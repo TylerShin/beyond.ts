62d583d7d05097b31d1f269d6668a6dd3a6e1d1b
Tylor Shin <shincode@icloud.com>
2016-10-16 15:01:42 +0900

    Added basic client/server rules

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
+import * as ReactDom from "react-dom";
 import * as ReactDOMServer from "react-dom/server";
 
 import { Hello } from "./components/hello";
 
-ReactDOMServer.renderToString(
-  <Hello compiler="TypeScript" framework="React" />
-);
+if (process.env.NODE_ENV) {
+  ReactDom.render(
+    <Hello compiler="TypeScript" framework="React" />,
+    document.getElementById('example')
+  );
+} else {
+  const HTML = ReactDOMServer.renderToString(
+    <Hello compiler="TypeScript" framework="React" />
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,32 @@
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    filename: "./dist/bundle.js",
-  },
-  devtool: "source-map",
+require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+module.exports = {
+  entry: [
+    'webpack/hot/dev-server',
+    './src/index.tsx',
+  ],
+  output: {
+    filename: './dist/bundle.js',
+  },
+  devtool: 'source-map',
   resolve: {
-    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
   module: {
-    loaders:
-      { test: /\.tsx?$/, loader: "ts-loader" }
+    loaders: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
     ],
     preLoaders: [
-      { test: /\.js$/, loader: "source-map-loader" }
+      { test: /\.js$/, loader: 'source-map-loader' }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
