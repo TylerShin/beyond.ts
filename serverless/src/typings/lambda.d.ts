538e3c53f3bac28597148cdca4d92ff629942e66
Tylor Shin <shincode@icloud.com>
2016-11-11 17:45:03 +0900

    WIP

---
+++ serverless/src/typings/lambda.d.ts
@@ -0,0 +1,64 @@
+// Type definitions for AWS Lambda
+// Definitions by: James Darbyshire (http://darb.io)
+
+/************************************************
+*                                               *
+*               AWS Lambda API                  *
+*                                               *
+************************************************/
+
+interface LambdaIntegrationResponse {
+  statusCode: number;
+  headers: {
+    'Content-Type': string
+  };
+  body: string;
+}
+
+// Context
+// http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+interface Context {
+    // Properties
+    functionName: string;
+    functionVersion: string;
+    invokedFunctionArn: string;
+    memoryLimitInMB: number;
+    awsRequestId: string;
+    logGroupName: string;
+    logStreamName: string;
+    identity?: CognitoIdentity;
+    clientContext?: ClientContext;
+
+    // Functions
+    succeed(result?: LambdaIntegrationResponse): void;
+    fail(error: Error): void;
+    done(error?: Error | null, result?: LambdaIntegrationResponse): void; // result must be JSON.stringifyable
+    getRemainingTimeInMillis(): number;
+}
+
+interface CognitoIdentity {
+    cognito_identity_id: string;
+    cognito_identity_pool_id: string;
+}
+
+interface ClientContext {
+    client: ClientContextClient;
+    Custom?: any;
+    env: ClientContextEnv;
+}
+
+interface ClientContextClient {
+    installation_id: string;
+    app_title: string;
+    app_version_name: string;
+    app_version_code: string;
+    app_package_name: string;
+}
+
+interface ClientContextEnv {
+    platform_version: string;
+    platform: string;
+    make: string;
+    model: string;
+    locale: string;
+}
