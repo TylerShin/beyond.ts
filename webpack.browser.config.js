de8411f783a527996b1464b24e347b6301c70656
Tylor Shin <shincode@icloud.com>
2018-02-11 11:44:31 +0900

    Add ESLint and ESLint setting && clean up webpack config files && Change svg loader to svg-sprite-loader && Upgrade React releative libraries

--- webpack.browser.config.js
+++ webpack.browser.config.js
@@ -1,73 +1,23 @@
-const webpack = require("webpack");
-const autoprefixer = require("autoprefixer");
-const BROWSER_BUNDLE_FILE_NAME = "bundleBrowser.js";
+const webpack = require('webpack');
+const originalWepbackConfig = require('./webpack.config');
+const BROWSER_BUNDLE_FILE_NAME = 'bundleBrowser.js';
 
-module.exports = {
-  entry: ["babel-polyfill", "./app/index.tsx"],
+const browserSpecificSetting = {
   output: {
     filename: `./dist/${BROWSER_BUNDLE_FILE_NAME}`,
   },
-  resolve: {
-    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-      },
-      {
-        test: /\.svg$/,
-        loader: "svg-inline-loader",
-        options: {
-          classPrefix: true,
-        },
-      },
-      {
-        test: /\.html$/,
-        use: ["raw-loader"],
-      },
-      {
-        test: /\.md?$/,
-        use: [{ loader: "html-loader" }, { loader: "markdown-loader" }],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: "isomorphic-style-loader" },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              localIdentName: "[name]__[local]__[hash:base64:3]",
-            },
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              plugins: function() {
-                return [require("precss"), require("autoprefixer"), require("postcss-flexbugs-fixes")];
-              },
-            },
-          },
-          { loader: "sass-loader" },
-        ],
-      },
-    ],
-  },
   node: {
-    fs: "empty",
-  },
-  externals: {
-    "react/lib/ExecutionEnvironment": true,
-    "react/lib/ReactContext": true,
-    "react/addons": true,
+    fs: 'empty',
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production"),
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
   ],
 };
+
+const webpackOptionsForBrowser = { ...originalWepbackConfig, ...browserSpecificSetting };
+
+module.exports = webpackOptionsForBrowser;
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,72 +1,25 @@
-const webpack = require("webpack");
-const autoprefixer = require("autoprefixer");
+const webpack = require('webpack');
+const originalWepbackConfig = require('./webpack.config');
 
-module.exports = {
-  entry: ["./app/index.tsx"],
+const nodeServerSpecificSettings = {
+  entry: ['./app/index.tsx'],
   output: {
-    libraryTarget: "commonjs",
-    library: "ssr",
-    filename: "./dist/bundle.js",
+    libraryTarget: 'commonjs',
+    library: 'ssr',
+    filename: './dist/bundle.js',
   },
-  target: "node",
-  resolve: {
-    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-      },
-      {
-        test: /\.svg$/,
-        loader: "svg-inline-loader",
-        options: {
-          classPrefix: true,
-        },
-      },
-      {
-        test: /\.html$/,
-        use: ["raw-loader"],
-      },
-      {
-        test: /\.md?$/,
-        use: [{ loader: "html-loader" }, { loader: "markdown-loader" }],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: "isomorphic-style-loader" },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              localIdentName: "[name]__[local]__[hash:base64:3]",
-            },
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              plugins: function() {
-                return [require("precss"), require("autoprefixer"), require("postcss-flexbugs-fixes")];
-              },
-            },
-          },
-          { loader: "sass-loader" },
-        ],
-      },
-    ],
-  },
-  externals: {
-    "react/lib/ExecutionEnvironment": true,
-    "react/lib/ReactContext": true,
-    "react/addons": true,
-  },
+  target: 'node',
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production"),
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
   ],
 };
+
+delete originalWepbackConfig.node;
+
+const nodeTargetWebpackOptions = { ...originalWepbackConfig, ...nodeServerSpecificSettings };
+
+module.exports = nodeTargetWebpackOptions;
